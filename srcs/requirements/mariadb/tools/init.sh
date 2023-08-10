3d89a51808aedc430689f4d812193fada8630581
marcocslima <marcocslima@gmail.com>
2023-08-10 03:06:01 -0300

    inception

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -12,7 +12,7 @@ mysql --user=root << _EOF_
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$WORDPRESS_PASSWORD';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASSWORD';
 FLUSH PRIVILEGES;
 _EOF_
 
