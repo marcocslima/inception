93b522ce9f87b0a9bdb8c8b615ae7f87235d56cd
marcocslima <marcocslima@gmail.com>
2023-08-10 02:59:40 -0300

    inception

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -2,16 +2,29 @@
 
 if [! -d "/var/lib/mysql/$WORDPRESS_DATABASE"]
 then
-
-/etc/init.d/mariadb start
-mysql -u root -p$ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $WORDPRESS_DATABASE; CREATE USER '$WORDPRESS_USER'@'%' IDENTIFIED BY '$WORDPRESS_PASSWORD'; GRANT ALL ON $WORDPRESS_DATABASE.* TO '$WORDPRESS_USER'@'%' IDENTIFIED BY '$WORDPRESS_PASSWORD'; FLUSH PRIVILEGES;"
-mysql -u root -p$ROOT_PASSWORD $WORDPRESS_DATABASE < /tmp/dump.sql
-mysql -u root -p$ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';"
-mysql -u root -p$ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}' WITH GRANT OPTION;"
-mysql -u root -p$ROOT_PASSWORD -e "FLUSH PRIVILEGES"
-sed -i "s/password =/password = ${ROOT_PASSWORD}/g" /etc/mysql/debian.cnf
-/etc/inid.d/mariadb stop
-
+    mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 fi
+
+mysqld_safe --datadir=/var/lib/mysql &
+sleep 5
+
+mysql --user=root << _EOF_
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$WORDPRESS_PASSWORD';
+FLUSH PRIVILEGES;
+_EOF_
+
+chown -R mysql:mysql /var/lib/mysql
+
+mysql --user=root --password=$ROOT_PASSWORD << _EOF_
+CREATE DATABASE IF NOT EXISTS $WORDPRESS_DATABASE;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$ROOT_PASSWORD';
+GRANT ALL PRIVILEGES ON $WORDPRESS_DATABASE.* TO '$WORDPRESS_USER'@'%' IDENTIFIED BY '$WORDPRESS_PASSWORD' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+_EOF_
+
+mysqladmin -uroot -p$ROOT_PASSWORD shutdown
 
 exec "$@"
