a69d7c83aa5322ee3b8b629622c5b6bb4237f230
marcocslima <marcocslima@gmail.com>
2023-08-18 16:51:55 -0300

    inception

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -4,8 +4,22 @@ rm -rf *.*
 
 wp core download --allow-root
 
-wp config create --allow-root --dbhost=$WORDPRESS_HOSTNAME --dbname=$WORDPRESS_DATABASE --dbuser=$WORDPRESS_USER --dbpass=$WORDPRESS_PASSWORD --locale=pt_BR
+wp config create --allow-root \
+                    --dbhost=$WORDPRESS_HOSTNAME \
+                    --dbname=$WORDPRESS_DATABASE \
+                    --dbuser=$WORDPRESS_USER \
+                    --dbpass=$WORDPRESS_PASSWORD
 
-wp core install --allow-root --url=$WP_URL --title="INCEPTION" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD  --admin_email=$WP_ADMIN_EMAIL
+wp config set --allow-root WP_HOME $DOMAIN_NAME
+wp config set --allow-root WP_SITEURL $DOMAIN_NAME
+
+wp core install --allow-root \
+                    --url=$DOMAIN_NAME \
+                    --title="INCEPTION" \
+                    --admin_user=$WP_ADMIN_USER \
+                    --admin_password=$WP_ADMIN_PASSWORD \
+                    --admin_email=$WP_ADMIN_EMAIL \
+                    --skip-email
+
 
 exec "$@"
